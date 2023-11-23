166d7302755e6478dd7d40d1d361f777c88882ed
vladkrasnikov <krasnikov.vladislav71@gmail.com>
2023-11-23 19:51:55 +0200

    Removed unused. Made scrollable and height change on expand.

--- src/app/modules/company-details/company-details.component.ts
+++ src/app/modules/company-details/company-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Company} from "../../core/models/company";
 
@@ -9,40 +9,14 @@ import {Company} from "../../core/models/company";
 })
 export class CompanyDetailsComponent implements OnInit {
   company!: Company;
-  @ViewChild('matCardDiv') matCardDiv: ElementRef | undefined
-  @Input() selected: boolean = false;
-  @Output() onClick: EventEmitter<number> = new EventEmitter<number>();
-  @Output() onDelete: EventEmitter<number> = new EventEmitter<number>();
-  @Output() onEdit: EventEmitter<void> = new EventEmitter<void>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.data.subscribe(
       (companyInfo: any) => {
-        console.log(companyInfo);
         this.company = companyInfo.data;
       });
-  }
-  open = false;
-
-  handleOnClick() {
-    // this.onClick.emit(this.company.id);
-  }
-
-  handleOnDelete(event: Event) {
-    event.stopPropagation();
-    // this.onDelete.emit(this.brand.id);
-  }
-
-  handleOnEdit(event: Event) {
-    event.stopPropagation();
-    this.onEdit.emit();
-    this.open = true;
-  }
-
-  handleCloseModal() {
-    this.open = false;
   }
 
   toggleDescription(brand: any): void {
